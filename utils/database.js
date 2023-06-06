e25ebb755bcd657c12b54f081193fbb1bb2f7edc
Montywilliams <3650@holbertonschool.com>
2023-06-06 17:16:43 -0600

    after setting up mongoDB on the back end and gstting db url and pass as env var the data base is coplete

--- utils/database.js
+++ utils/database.js
@@ -3,5 +3,22 @@ import mongoose from 'mongoose';
 let isConnected = false;
 
 export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
 
-}ga 
+    if(isConnected) {
+        console.log('MongoDB is alredy connected Bruh')
+        return;
+    }
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "share_prompt",
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+        isConnected = true;
+        console.log('MongoDB connected ')
+    } catch (error) {
+        console.log(error)
+    } 
+}
