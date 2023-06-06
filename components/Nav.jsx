aeeba14c30a6c453a0c7840abc40844dc004647e
Montywilliams <3650@holbertonschool.com>
2023-06-06 10:31:31 -0600

    utilize getProviders function to return providers object then map through to return our provider name

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -52,7 +52,16 @@ function Nav() {
               </Link>
           </div>): (
             <>
-              {/* Button to sign in */}
+              {providers &&
+                Object.values(providers).map(provider => (
+                  <button
+                    type="button"
+                    key={provider.name}
+                    onClick={() => signIn(provider.id)}
+                    className="black_btn"
+                  >Sign In</button>
+                ))
+              }
             </>
           )}
       </div>
