8458f6a1ff7b50b4f8fd67de007d4234272825ee
Montywilliams <3650@holbertonschool.com>
2023-06-06 09:01:55 -0600

    import logo: add nav styling

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,7 +7,18 @@ import {signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Prompt Mastery Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Prompt Mastery</p>
+      </Link>
+    </nav>
   )
 }
 
