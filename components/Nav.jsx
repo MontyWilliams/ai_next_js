40d3caa010eba5ca87aa5dda4cf30c0e1d0d15cd
Montywilliams <3650@holbertonschool.com>
2023-06-07 08:10:13 -0600

    use session to check if user is logged in

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ import {signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 
 function Nav() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null)
   const [toggleDropdown, setToggleDropdown] = useState(false)
@@ -34,7 +34,7 @@ function Nav() {
       
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt"
               className="black_btn">
@@ -69,7 +69,7 @@ function Nav() {
 
       {/* Mobile App */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
               src="/assets/images/logo.svg"
