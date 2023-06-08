c0b0c8933ad82475c550b4bd690628e4545c2662
Montywilliams <3650@holbertonschool.com>
2023-06-07 21:51:34 -0600

    add label for tag input

--- components/Form.jsx
+++ components/Form.jsx
@@ -29,7 +29,22 @@ function Form({type, post, setPost, submitting, handleSubmit}) {
           />
           </label>
 
-         
+          <label>
+            <span className="font-satoshi font-semibold text-base text-gray-700">
+              Tag{' '}
+              <span className="font-normal">(#product, #webdevelopment, #idea)</span>
+            </span>
+            <input
+            value={post.tag}
+            onChange={(e) => setPost({
+              ...post,
+              tag: e.target.value,
+            })}
+            placeholder="#tag"
+            required
+            className="form_input"
+          />
+          </label>
         </form>
     </section>
   )
