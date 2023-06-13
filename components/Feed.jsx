35566103a6bae5a2b8381cf613208db9ad019213
Montywilliams <3650@holbertonschool.com>
2023-06-13 09:49:49 -0600

    correct syntax

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -7,7 +7,13 @@ import PromptCard from './PromptCard';
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
     <div className="mt-16 prompt_layout">
-      
+      {data.map((post) => (
+          <PromptCard 
+            key={post._id}
+            post={post}
+            handleTagClick={handleTagClick}
+          />
+        ))}
     </div>
   )
 }
@@ -42,7 +48,10 @@ function Feed() {
         />
       </form>
 
-      <PromptCardList />
+      <PromptCardList
+        data={posts}
+        handleTagClick={() => {}}
+      />
     </section>
   )
 }
