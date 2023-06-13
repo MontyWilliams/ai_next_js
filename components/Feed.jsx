e636d864f4b6de012efe9866d7f8a2f92d1e32c5
Montywilliams <3650@holbertonschool.com>
2023-06-13 08:54:04 -0600

    c&s section, form input: create const to handle state

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+'use client'
+
+import { useState, useEffect } from 'react';
+
+import PromptCard from './PromptCard';
 
 function Feed() {
+  const [ searchText, setSearchText ] = useState('')
+  const handleSearchChange = ( ) => {
+
+  }
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or ausername"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+    </section>
   )
 }
 
