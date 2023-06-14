5ede64ab085064183e0d8b07d2a1c62cc7c11aaa
Montywilliams <3650@holbertonschool.com>
2023-06-14 12:48:45 -0600

    add condition to return 404 if prompt not found: correction-return prompt instead of prompts

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -6,8 +6,9 @@ export const GET = async (request, { params }) => {
         await connectToDB();
 
         const prompt = await Prompt.findById(params.Id).populate('creator');
+        if(!prompt) return new Response("Prompt not found", { status: 404 })
         
-        return new Response(JSON.stringify(prompts), { status: 200 })
+        return new Response(JSON.stringify(prompt), { status: 200 })
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 })
     }
