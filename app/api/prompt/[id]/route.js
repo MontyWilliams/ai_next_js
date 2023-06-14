7200ea739da574eadef6d1966192a02c90c7f3d4
Montywilliams <3650@holbertonschool.com>
2023-06-14 14:36:56 -0600

    correct typo: id

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -5,8 +5,8 @@ export const GET = async (request, { params }) => {
     try {
         await connectToDB();
 
-        const prompt = await Prompt.findById(params.Id).populate('creator');
-        if(!prompt) return new Response("Prompt not found", { status: 404 })
+        const prompt = await Prompt.findById(params.id).populate('creator');
+        if (!prompt) return new Response("Prompt not found", { status: 404 })
         
         return new Response(JSON.stringify(prompt), { status: 200 })
     } catch (error) {
